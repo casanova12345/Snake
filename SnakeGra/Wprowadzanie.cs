977a6191b6deb45c97577d1e541f2ce1f20b177c
casanova12345 <krukowskim123@gmail.com>
2016-12-05 20:27:42 +0100

    4

---
+++ SnakeGra/Wprowadzanie.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+
+namespace SnakeGra
+{
+    class Wprowadzanie
+    {
+        private static Hashtable Przyciski = new Hashtable();
+
+        public static bool Klawisze(Keys klawisz)
+        {
+            if (Przyciski[przycisk] == null)
+            {
+                return false;
+            }
+
+            return (bool)Przyciski[klawisz];
+        }
+
+        public static void ZmianaStatusu(Keys key, bool status)
+        {
+            Przyciski[klawisz] = status;
+        }
+    }
+}
